e9a29ac192c8b5ab9212deeb19fcd6f270766af4
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-18 23:24:56 -0500

    Exercise currency convert pesos mexican

--- exercisePython/currency.py
+++ exercisePython/currency.py
@@ -1,5 +1,4 @@
 pesos = float(input('Ingresa pesos colombianos: '))
 value_dollar = 3875
 dollars = pesos / value_dollar
-
-print(f'Pesos colombianos: {pesos} = {dollars} dolares')
+print(f'Pesos colombianos: {pesos} = {round(dollars, 2)} dolares')
---
+++ exercisePython/pesos.py
@@ -0,0 +1,4 @@
+pesos = float(input('Ingresa pesos mexicanos: '))
+value_dollar = 0.050
+dollars = pesos * value_dollar
+print(f'Pesos mexicanos: {pesos} = {round(dollars, 2)} dolares')
