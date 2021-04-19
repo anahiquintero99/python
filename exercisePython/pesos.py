714271966a87b3179df8c4c5d125725e0369f827
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-18 23:29:55 -0500

    Exercise currency convert dollars

--- exercisePython/pesos.py
+++ exercisePython/pesos.py
@@ -2,3 +2,8 @@ pesos = float(input('Ingresa pesos mexicanos: '))
 value_dollar = 0.050
 dollars = pesos * value_dollar
 print(f'Pesos mexicanos: {pesos} = {round(dollars, 2)} dolares')
+
+dollars = float(input('Ingresa dolares: '))
+pesos = 19.90
+pesos = dollars * pesos
+print(f'Dolares: {dollars} = {round(pesos, 2)} pesos')
