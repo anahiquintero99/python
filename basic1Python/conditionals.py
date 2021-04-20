dea7bc05db2e58eae5847e23a7e67872947109f9
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-19 20:29:53 -0500

    Exercise to adult

---
+++ basic1Python/conditionals.py
@@ -0,0 +1,16 @@
+age = int(input('Ingresa edad: '))
+adult = 17
+
+if age >= adult:
+    print('Eres mayor de edad.')
+else:
+    print('No eres mayor de edad.')
+
+
+number = int(input('Ingresa numero: '))
+if number < 5:
+    print('Es menor a 5')
+elif number == 5:
+    print('Es igual a 5')
+else:
+    print('Es menos a 5')
