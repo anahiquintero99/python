e61cbe4707c715f1db25055d12aece133db143dc
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-19 21:45:34 -0500

    add details to exercise converter currency

--- currency_converter.py
+++ currency_converter.py
@@ -9,23 +9,26 @@ Welcome to the currency converter
 
 option = int(input(menu))
 
+
+def converter(value_pesos, country):
+    pesos = float(input('Enter amount: '))
+    dollars = value_pesos * pesos
+    print(f'{country} Pesos: {pesos} = {round(dollars, 2)} dollars')
+
+
 if option == 1:
-    mexican_pesos = float(input('Enter amount: '))
-    value_mexican = 0.050
-    dollars = value_mexican * mexican_pesos
-    print(f'Mexican pesos: {mexican_pesos} = {round(dollars, 2)} dollars')
+    value_pesos = 0.050
+    country = 'Mexican'
+    converter(value_pesos, country)
 
 elif option == 2:
-    colombian_pesos = float(input('Enter amount: '))
-    value_colombian = 0.00028
-    dollars = value_colombian * colombian_pesos
-    print(f'Colombian pesos: {colombian_pesos} = {round(dollars, 2)} dollars')
+    value_pesos = 0.00028
+    country = 'Colombian'
+    converter(value_pesos, country)
 
 elif option == 3:
-    argentine_pesos = float(input('Enter amount: '))
-    value_argentine = 0.011
-    dollars = value_argentine * argentine_pesos
-    print(f'Mexican pesos: {argentine_pesos} = {round(dollars, 2)} dollars')
-
+    value_pesos = 0.011
+    country = 'Argentine'
+    converter(value_pesos, country)
 else:
     print('Enter a correct option.')
---
+++ fuction.py
@@ -0,0 +1,8 @@
+def print_messages():
+    print('This is a messages.')
+    print('I am learning to programming.')
+
+
+print_messages()
+print_messages()
+print_messages()
