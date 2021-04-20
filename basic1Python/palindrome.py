63eab23a0cc3b01692fc94b47829d55fc0d41b35
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-19 22:36:37 -0500

    Details to exercise palindrome words

--- basic1Python/palindrome.py
+++ basic1Python/palindrome.py
@@ -1,9 +1,22 @@
+def palindrome(word):
+    word = word.replace(' ', '')
+    word = word.lower()
+    word_backwards = word[::-1]
+    if word == word_backwards:
+        return True
+    else:
+        return False
 
-word = input('\nEnter Word: ')
-word_backwards = word[::-1]
-print(f'\nWord: {word_backwards}')
 
-if word == word_backwards:
-    print('\nThe word is palindrome.')
-else:
-    print('\nThe word is not palindrome')
+def run():
+    word = input('\nEnter Word: ')
+    is_palindrome = palindrome(word)
+
+    if is_palindrome == True:
+        print('\nThe word is palindrome.')
+    else:
+        print('\nThe word is not palindrome')
+
+
+if __name__ == '__main__':
+    run()
