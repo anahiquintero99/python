0d2746ee3f51d929b7174963ae98dc1613f320c2
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-25 23:21:01 -0500

    details

--- basic1Python/number_primer.py
+++ basic1Python/number_primer.py
@@ -1,7 +1,24 @@
+def number_prime(number):
+    contador = 0
+
+    for i in range(1, number + 1):
+        if i == 1 or i == number:
+            continue
+        if number % i == 0:
+            contador += 1
+
+    if contador == 0:
+        return True
+    else:
+        return False
 
 
 def run():
-    pass
+    number = int(input('Enter number: '))
+    if number_prime(number):
+        print('It is prime')
+    else:
+        print('It is not prime')
 
 
 if __name__ == '__main__':
