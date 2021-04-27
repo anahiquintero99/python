b6bad98f6993c1a851be8202787619e975256298
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-26 20:04:32 -0500

    details

--- basic1Python/my_prime.py
+++ basic1Python/my_prime.py
@@ -1,19 +1,23 @@
 def number_prime(number):
-    if number % 1 == 0:
-        for i in range(2, number):
-            if number % i == 0:
-                return True
-                break
-            else:
-                return False
+    pass
 
 
 def run():
     number = int(input('Enter number: '))
-    if number_prime(number) == True:
-        print('No es primo')
-    else:
-        print('Es primo')
+    # if number == 1:
+    # print('No es primo')
+    # elif number_prime(number) == True:
+    # print('No es primo')
+    # else:
+    # print('Es primo')
+    if number % 1 == 0:
+        for i in range(2, number):
+            value = int(number % i)
+            if value == 0:
+                print('No es primo')
+                break
+            else:
+                print('Es primo')
 
 
 if __name__ == '__main__':
