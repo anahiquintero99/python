b471571ebf7ae24a692fddcded1b97f76b14cf25
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-26 20:15:14 -0500

    Finally exercise about number prime

--- basic1Python/my_prime.py
+++ basic1Python/my_prime.py
@@ -1,23 +1,22 @@
 def number_prime(number):
-    pass
-
-
-def run():
-    number = int(input('Enter number: '))
-    # if number == 1:
-    # print('No es primo')
-    # elif number_prime(number) == True:
-    # print('No es primo')
-    # else:
-    # print('Es primo')
     if number % 1 == 0:
         for i in range(2, number):
             value = int(number % i)
             if value == 0:
-                print('No es primo')
+                return True
                 break
             else:
-                print('Es primo')
+                continue
+
+
+def run():
+    number = int(input('Enter number: '))
+    if number == 1:
+        print('No es primo')
+    elif number_prime(number) == True:
+        print('No es primo')
+    else:
+        print('Es primo')
 
 
 if __name__ == '__main__':
