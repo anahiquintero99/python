68e9f39b92efeba184703846260321689ac99b8a
Anahi Quintero <anitaquintero99@gmail.com>
2021-05-05 01:06:59 -0500

    New exercise , alphabetic and numerics

--- interpython/numbers.py
+++ interpython/numbers.py
@@ -1,7 +1,36 @@
 
-s = 'qA2'
-print(s.isalnum())
-print (s.isalpha())
-print(s.isdigit())
-print(s.islower())
-print(s.isupper())
+
+def isalum_value(s):
+  list_s = list(s)
+  for i in list_s:
+    if i.isalnum() == True:
+      return True
+    else:
+      return False
+
+def isalpha_value(s):
+  list_s = list(s)
+  for i in list_s:
+    if i.isalpha() == True:
+      return True
+    else:
+      return False
+
+def isdigit_value(s):
+  list_s = list(s)
+  for i in list_s:
+    if i.isdigit() == True:
+      return True
+    else:
+      return False
+      continue
+
+
+def run():
+  s = 'qA2'
+  print(isalum_value(s))
+  print(isalpha_value(s))
+  print(isdigit_value(s))
+
+if __name__ == '__main__':
+  run()
