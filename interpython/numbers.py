6d26a5f0f2c3c0a443627e70f784e0177aadd32a
Anahi Quintero <anitaquintero99@gmail.com>
2021-05-16 17:44:06 -0500

    add any and map

--- interpython/numbers.py
+++ interpython/numbers.py
@@ -1,36 +1,10 @@
-
-
-def isalum_value(s):
-  list_s = list(s)
-  for i in list_s:
-    if i.isalnum() == True:
-      return True
-    else:
-      return False
-
-def isalpha_value(s):
-  list_s = list(s)
-  for i in list_s:
-    if i.isalpha() == True:
-      return True
-    else:
-      return False
-
-def isdigit_value(s):
-  list_s = list(s)
-  for i in list_s:
-    if i.isdigit() == True:
-      return True
-    else:
-      return False
-      continue
-
-
 def run():
-  s = 'qA2'
-  print(isalum_value(s))
-  print(isalpha_value(s))
-  print(isdigit_value(s))
+  text = 'qA2'
+  print(any(map(lambda x: x.isalnum(), text)))
+  print(any(map(lambda x: x.isalpha(), text)))
+  print(any(map(lambda x: x.isdigit(), text)))
+  print(any(map(lambda x: x.islower(), text)))
+  print(any(map(lambda x: x.isupper(), text)))
 
 if __name__ == '__main__':
   run()
