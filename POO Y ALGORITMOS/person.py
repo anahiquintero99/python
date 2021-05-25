d800ee5e63683225ad146b229aca5c25877cdf17
Anahi Quintero <anitaquintero99@gmail.com>
2021-05-24 20:00:40 -0500

    New ejercises to instances

--- POO Y ALGORITMOS/person.py
+++ POO Y ALGORITMOS/person.py
@@ -8,3 +8,9 @@ class Person:
   
   def greeting(self, other_person):
     return f'Hola {other_person.name} me llamo {self.name}'
+ 
+if __name__ == '__main__':
+    person_1 = ('Anahi', 22)
+    person_2 = ('Daniela', 7)
+    
+    print(person_1.greeting(person_2))
